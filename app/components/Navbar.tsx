fe62a0c8c978e5518cc7d5e8b2295302c79525a3
Fardeen <154688163+syedfardeenjeelani@users.noreply.github.com>
2024-06-27 00:25:48 +0530

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState, useMemo } from "react";
-import { Input, Form, message, Select, Button } from "antd";
+import { Input, Form, message, Select, Button, Row, Col } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState, AppDispatch } from "../redux/store";
 import data from "../data/vehicle_data - Copy.json";
@@ -52,38 +52,45 @@ export const Navbar: React.FC = () => {
   }, [dispatch]);
 
   return (
-    <Form style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: 10 }} className="w-full">
-      <div style={{ display: "flex", gap: 10, marginBottom: 10 }}>
-        <Input
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
-          placeholder="Search"
-          style={{ width: 200 }}
-        />
-        <Select
-          value={brandFilter}
-          onChange={setBrandFilter}
-          style={{ width: 150 }}
-          placeholder="Brand"
-        >
-          <Option value="">All Brands</Option>
-          {brands.map(brand => (
-            <Option key={brand} value={brand}>{brand}</Option>
-          ))}
-        </Select>
-        <Select
-          value={typeFilter}
-          onChange={setTypeFilter}
-          style={{ width: 150 }}
-          placeholder="Type"
-        >
-          <Option value="">All Types</Option>
-          {types.map(type => (
-            <Option key={type} value={type}>{type}</Option>
-          ))}
-        </Select>
-        <Button onClick={handleClear}>Clear Filters</Button>
-      </div>
+    <Form className="w-95% max-w-[95%] mx-auto p-4">
+      <Row gutter={[16, 16]} justify="center">
+        <Col  sm={12} md={6} lg={5}>
+          <Input
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            placeholder="Search"
+          />
+        </Col>
+        <Col  sm={12} md={6} lg={5}>
+          <Select
+            value={brandFilter}
+            onChange={setBrandFilter}
+            style={{ width: '95%' }}
+            placeholder="Brand"
+          >
+            <Option value="">All Brands</Option>
+            {brands.map(brand => (
+              <Option key={brand} value={brand}>{brand}</Option>
+            ))}
+          </Select>
+        </Col>
+        <Col sm={12} md={6} lg={5}>
+          <Select
+            value={typeFilter}
+            onChange={setTypeFilter}
+            style={{ width: '95%' }}
+            placeholder="Type"
+          >
+            <Option value="">All Types</Option>
+            {types.map(type => (
+              <Option key={type} value={type}>{type}</Option>
+            ))}
+          </Select>
+        </Col>
+        <Col sm={12} md={6} lg={5}>
+          <Button onClick={handleClear} style={{ width: '95%' }}>Clear Filters</Button>
+        </Col>
+      </Row>
     </Form>
   );
 };
