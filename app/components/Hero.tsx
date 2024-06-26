53ae9d3fcc5716de4d63ba203656cb1b39c7ad49
Fardeen <154688163+syedfardeenjeelani@users.noreply.github.com>
2024-06-27 00:12:59 +0530

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -42,7 +42,7 @@ export const Hero: React.FC = () => {
 
   if (loading) {
     return (
-      <div className="flex justify-center items-center h-[100vh]">
+      <div style={{ display: 'flex', justifyContent: 'center' , height:'100vh'}} className="flex justify-center items-center h-[100vh]">
         <Spin size="large" tip="Loading vehicles..." />
       </div>
     );
